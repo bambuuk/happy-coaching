24e6dc9c3afabd195a3ec6b4155b370111535a04
Ivan <vanyok.korobka@ukr.net>
2024-03-11 13:47:33 +0200

    working start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Promo from "./components/Promo";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Promo />
+    </>
+  );
+}
+
+export default App;
